629f6725cf4fb0f02b57049c39f170ffdbb549b1
awanat-olawale <olawaleawanat05@gmail.com>
2025-06-28 04:48:20 +0100

    feat: MySQL connector setup - Create alx_book_store database if it doesn't exist

---
+++ MySQLServer.py
@@ -0,0 +1,34 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+try:
+    # Connect to MySQL Server
+    connection = mysql.connector.connect(
+        host =os.getenv('DB_HOST'),
+        user = os.getenv('DB_USER'),
+        password = os.getenv('DB_PASSWORD'),
+        # database="alx_book_store"
+    )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+        # cursor.execute("SELECT * FROM Books")
+
+        # books = cursor.fetchall()
+
+        # for i in books:
+        #     print (i)
+    
+except Exception as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
