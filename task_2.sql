6f2d33512c0c45c3d58f3fc2b09fef1a858540c8
awanat-olawale <olawaleawanat05@gmail.com>
2025-06-28 09:35:31 +0100

    feat: Task 2 - Create Entity and Attribute tables

--- task_2.sql
+++ task_2.sql
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_details_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     unit_price DECIMAL(10, 2),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
